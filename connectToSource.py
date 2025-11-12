b7ebd6086d1a7a6487ebe8bc748ee26b19c2ec5a
Stryla <mgeist79@gmail.com>
2025-11-12 16:16:18 +0000

    Testing read

---
+++ connectToSource.py
@@ -0,0 +1,11 @@
+#This space is to pull in data for project
+
+#libraries
+import numpy as np
+import pandas as pd
+  
+#Set up Secure connection
+
+#import data into pandas dataframe
+
+#Push data to next notebook for transformations6
