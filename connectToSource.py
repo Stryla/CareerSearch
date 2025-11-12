1543236548aa0c86b6d092c7a1f27ec5bbaa6c93
Stryla <mgeist79@gmail.com>
2025-11-12 16:22:28 +0000

    Adding load file

--- connectToSource.py
+++ connectToSource.py
@@ -3,9 +3,19 @@
 #libraries
 import numpy as np
 import pandas as pd
-  
+import os  
+
+#Location and testing variables
+#print(os.getcwd())
 #Set up Secure connection
 
 #import data into pandas dataframe
+#Setting file path to a string variable
+file_path = "Job_Application_Tracker.csv"
+print(file_path)
+applicationsStatus = pd.read_csv(file_path)
 
-#Push data to next notebook for transformations6
+#Push data to next notebook for transformations
+headers = applicationsStatus.columns.tolist()
+print
+#applicationsStatus.head()
